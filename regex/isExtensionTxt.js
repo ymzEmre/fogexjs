bc9b5d9c783401b71955513b51368184a588c43f
ymzEmre <dev.yilmazemre@gmail.com>
2022-04-20 00:10:19 +0300

    Add new regex pattern `isExtensionTxt`

---
+++ regex/isExtensionTxt.js
@@ -0,0 +1,5 @@
+let patternExtensionTxt = new RegExp(/^(([a-zA-Z]:)|(\\{2}\w+)\$?)(\\(\w[\w ]*))+\.(txt|TXT)$/);
+
+const isExtensionTxt = (value) => {
+  return patternExtensionTxt.test(value);
+};
