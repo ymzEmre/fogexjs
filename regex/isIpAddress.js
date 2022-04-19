1612a458c83c467bbf8673a2dd9235d79d835931
ymzEmre <dev.yilmazemre@gmail.com>
2022-04-20 00:11:30 +0300

    Add new regex pattern `isIpAddress`

---
+++ regex/isIpAddress.js
@@ -0,0 +1,7 @@
+let patternIpAddress = new RegExp(
+  /^((0|1[0-9]{0,2}|2[0-9]?|2[0-4][0-9]|25[0-5]|[3-9][0-9]?)\.){3}(0|1[0-9]{0,2}|2[0-9]?|2[0-4][0-9]|25[0-5]|[3-9][0-9]?)$/
+);
+
+const isIpAddress = (value) => {
+  return patternIpAddress.test(value);
+};
