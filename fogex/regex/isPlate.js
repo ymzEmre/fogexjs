53e8969ebc4ee26fd826e8d672235a4e5ae8cdc0
ymzEmre <dev.yilmazemre@gmail.com>
2022-04-21 03:14:48 +0300

    Edit fucntion and variable name in isPlate

--- fogex/regex/isPlate.js
+++ fogex/regex/isPlate.js
@@ -1,7 +1,7 @@
-let plateRule = new RegExp(/^(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2,3}))$/);
+let patternPlate = new RegExp(/^(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2,3}))$/);
 
 const isPlate = (value) => {
-  return plateRule.test(value);
+  return patternPlate.test(value);
 };
 
 module.exports = isPlate;
