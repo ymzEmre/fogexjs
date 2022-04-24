5526fc21689800ae91fcca809ca6f34174645f4e
ahmet021 <ahmetozkan021@gmail.com>
2022-04-24 21:41:21 +0300

    add pattern rule

--- fogex/regex/isPlate.js
+++ fogex/regex/isPlate.js
@@ -1,7 +1,15 @@
-let patternPlate = new RegExp(/^(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2,3}))$/);
+const plates = {
+  TR: /^(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2,3}))$/,
+  US: /^B[A-Z]{2}-[0-9]{3}$/,
+};
 
-const isPlate = (value) => {
-  return patternPlate.test(value);
+const isPlate = (value, countyCode) => {
+  let patternPlate = new RegExp(plates[countyCode]);
+  if (value == null || patternPlate == "/(?:)/") {
+    return false;
+  } else {
+    return patternPlate.test(value);
+  }
 };
 
 module.exports = isPlate;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const fogex = require('./fogex');
+const fogex = require("./fogex");
+
+let isTrPhone = fogex.isPhoneNumber("+905357894520", "TR");
+console.log("tr phone: " + isTrPhone);
+
+let isUsPhone = fogex.isPhoneNumber("+1 800 5551212", "US");
+console.log("us phone: " + isUsPhone);
