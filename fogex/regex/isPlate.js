b3f6a0999eda7608b1319f919987168392f483e2
ahmet021 <ahmetozkan021@gmail.com>
2022-04-25 00:28:04 +0300

    add: patternRules phoneNumber, pin, plate

--- fogex/regex/isPlate.js
+++ fogex/regex/isPlate.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-const plates = {
-=======
 const patternPlates = {
->>>>>>> fogexjs-master
   TR: /^(0[1-9]|[1-7][0-9]|8[01])(([A-Z])(\d{4,5})|([A-Z]{2})(\d{3,4})|([A-Z]{3})(\d{2,3}))$/,
   US: /^B[A-Z]{2}-[0-9]{3}$/,
 };
 
 const isPlate = (value, countyCode) => {
-<<<<<<< HEAD
-  let patternPlate = new RegExp(plates[countyCode]);
-  if (value == null || patternPlate == "/(?:)/") {
-=======
   let patternPlate = new RegExp(patternPlates[countyCode]);
   if (value == null || patternPlate == '/(?:)/') {
->>>>>>> fogexjs-master
     return false;
   } else {
     return patternPlate.test(value);
--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-const fogex = require("./fogex");
-
-let isTrPhone = fogex.isPhoneNumber("+905357894520", "TR");
-console.log("tr phone: " + isTrPhone);
-
-let isUsPhone = fogex.isPhoneNumber("+1 800 5551212", "US");
-console.log("us phone: " + isUsPhone);
-=======
 const fogex = require('./fogex');
->>>>>>> fogexjs-master
